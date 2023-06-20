9ed053c56f1ad719017ce4c63715a27fa689a0bf
Jakub Plecháč <plechac.k@gmail.com>
2023-06-20 19:36:25 +0200

    created project

---
+++ lib/empty_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: Center(child: Container(child: Text("Push Notification")))));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'empty_page.dart';
+import 'firebase_options.dart';
+import 'home_page.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.instance.getToken().then((value) {
+    print("getToken : $value");
+  });
+
+  FirebaseMessaging.onMessageOpenedApp.listen(
+    (RemoteMessage message) async {
+      print("onMessageOpenedApp: $message");
+      Navigator.pushNamed(navigatorKey.currentState!.context, '/push-page',
+          arguments: {
+            "message",
+            json.encode(message.data),
+          });
+    },
+  );
+
+  FirebaseMessaging.instance.getInitialMessage().then(
+    (RemoteMessage? message) {
+      if (message != null) {
+        Navigator.pushNamed(navigatorKey.currentState!.context, '/push-page',
+            arguments: {
+              "message": json.encode(message.data),
+            });
+      }
+    },
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  runApp(const MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("_firebaseMessagingBackgroundHandler: $message");
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      navigatorKey: navigatorKey,
+      routes: {
+        '/': (context) => const EmptyPage(),
+        '/push-page': (context) => const HomePage(),
+      },
+    );
+  }
+}
